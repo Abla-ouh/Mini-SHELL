6cc67114e3b6870b324aeadaa1bf9a2b2a12f810
Abla Ouhaga <abouhaga@e2r5p9.1337.ma>
2022-09-25 16:06:40 +0100

    manual h FIX

--- inc/minishell.h
+++ inc/minishell.h
@@ -3,31 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: midfath <midfath@student.42.fr>            +#+  +:+       +#+        */
+/*   By: abouhaga <abouhaga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 16:18:26 by abouhaga          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2022/09/25 15:41:37 by abouhaga         ###   ########.fr       */
-=======
-/*   Updated: 2022/09/25 15:18:23 by midfath          ###   ########.fr       */
->>>>>>> 410e567da2420f88fac4471f492af5fa2c0aa19d
+/*   Updated: 2022/09/25 16:06:17 by abouhaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
-
 # include "../libft/libft.h"
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>
-<<<<<<< HEAD
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-=======
 
 // typedef struct s_envp
 // {
